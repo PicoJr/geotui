41fb68076d5b57c41a94825c94a8c15044ac6c57
PicoJr <picojr_dev@gmx.com>
2020-08-11 21:54:41 +0200

    Add more controls

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ mod world;
 
 use crate::custom_map::{CustomMap, CustomMapResolution};
 use crate::util::event::{Event, Events};
-use nalgebra::Similarity2;
+use nalgebra::{Similarity2, Vector2};
 use std::error::Error;
 use std::io;
 use termion::{event::Key, input::MouseTerminal, raw::IntoRawMode, screen::AlternateScreen};
@@ -13,6 +13,18 @@ use tui::style::Color;
 use tui::widgets::canvas::Canvas;
 use tui::widgets::{Block, Borders};
 use tui::Terminal;
+
+fn control(key: &Key) -> Similarity2<f64> {
+    match key {
+        Key::PageUp => Similarity2::from_scaling(1.1),
+        Key::PageDown => Similarity2::from_scaling(0.9),
+        Key::Left => Similarity2::new(Vector2::new(2.0, 0.0), 0.0, 1.0),
+        Key::Right => Similarity2::new(Vector2::new(-2.0, 0.0), 0.0, 1.0),
+        Key::Down => Similarity2::new(Vector2::new(0.0, 2.0), 0.0, 1.0),
+        Key::Up => Similarity2::new(Vector2::new(0.0, -2.0), 0.0, 1.0),
+        _ => Similarity2::identity(),
+    }
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
     // Terminal initialization
@@ -45,14 +57,12 @@ fn main() -> Result<(), Box<dyn Error>> {
         })?;
 
         if let Event::Input(key) = events.next()? {
-            if key == Key::Char('q') {
-                break;
-            }
-            if key == Key::PageUp {
-                transform.set_scaling(transform.scaling() * 1.1);
-            }
-            if key == Key::PageDown {
-                transform.set_scaling(transform.scaling() * 0.9);
+            match key {
+                Key::Char('q') => break,
+                _ => {
+                    let control_transform = control(&key);
+                    transform = transform * control_transform;
+                }
             }
         }
     }
